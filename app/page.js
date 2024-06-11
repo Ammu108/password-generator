9ffe50b999fca9c406eceae9e24c126590d288fa
Apple <apple@Apples-MacBook-Pro.local>
2024-06-11 20:17:52 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,144 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from 'react'
 
-export default function Home() {
+const page = () => {
+
+  const [checksc, setChecksc] = useState(false)
+  const [checklc, setChecklc] = useState(false)
+  const [checkuc, setCheckuc] = useState(false)
+  const [checknc, setChecknc] = useState(false)
+
+  const [range, setRange] = useState('')
+  const [outputValue, setOutputValue] = useState('')
+  const [showNotificationLength, setNotificationsLength] = useState(false)
+  const [showNotificationCheckbox, setNotificationsCheckbox] = useState(false)
+  const [showNotificationCopied, setNotificationsCopied] = useState(false)
+
+
+  const sc = '!~@#$%^&*()><?}{';
+  const lc = 'abcdefghijklmnopqrstuvwxyz';
+  const uc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const nc = '1234567890';
+
+  let passwordGenerator = () => {
+
+    if (range < 6) {
+      setNotificationsLength(true);
+      setTimeout(() => {
+        setNotificationsLength(false);
+      }, 2000);
+      return;
+    }
+    setNotificationsLength(false);
+
+    let finalOutput = '';
+
+    if (checksc || checklc || checknc || checkuc) {
+      let charSet = '';
+      if (checksc) {
+        charSet += sc;
+      }
+      if (checklc) {
+        charSet += lc;
+      }
+      if (checknc) {
+        charSet += nc;
+      }
+      if (checkuc) {
+        charSet += uc;
+      }
+
+      for (let i = 0; i < range; i++) {
+        finalOutput += charSet.charAt(Math.floor(Math.random() * charSet.length))
+      }
+      setOutputValue(finalOutput);
+
+    } else {
+      // alert('please select anyone....')
+      setNotificationsCheckbox(true);
+      setTimeout(() => {
+        setNotificationsCheckbox(false);
+      }, 2000);
+
+    }
+  }
+
+  const copyPassword = () => {
+    navigator.clipboard.writeText(outputValue)
+    setNotificationsCopied(true);
+    setTimeout(() => {
+      setNotificationsCopied(false);
+    }, 2000);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="container">
+        <div className="inbox">
+          <div className="inbox-content">
+            <h2 className='heading'>Password Creator</h2>
+
+            <div className="top-section">
+              <input type='text' readOnly value={outputValue} className='password-input'></input>
+              <div className="icon">
+                <i className="fa-regular fa-clone" onClick={copyPassword} style={{ cursor: 'pointer' }}></i>
+              </div>
+            </div>
+
+            <div className="range">
+              <h3 className="range-text">Password Length</h3>
+              <input type='number' min={1} max={20} value={range} onChange={(e) => setRange(e.target.value)} className='range-number'></input>
+            </div>
+
+            <div className="check-boxes">
+              <label className="capital-text">Upper Case</label>
+              <input type="checkbox" checked={checkuc} onChange={() => setCheckuc(!checkuc)} className='checkbox' />
+            </div>
+
+            <div className="check-boxes">
+              <label className="capital-text">Lower Case</label>
+              <input type="checkbox" checked={checklc} onChange={() => setChecklc(!checklc)} className='checkbox' />
+            </div>
+
+            <div className="check-boxes">
+              <label className="capital-text">Numbers</label>
+              <input type="checkbox" checked={checknc} onChange={() => setChecknc(!checknc)} className='checkbox' />
+            </div>
+
+            <div className="check-boxes">
+              <label className="capital-text">Symbols</label>
+              <input type="checkbox" checked={checksc} onChange={() => setChecksc(!checksc)} className='checkbox' />
+            </div>
+
+            <div className='generator'>
+              <button className='gen-btn' onClick={passwordGenerator}>Generate Password</button>
+            </div>
+          </div>
+        </div>
+
+        <div className={`notifications-length ${showNotificationLength ? " active-notifications-length " : ''}`}>
+          <div className="warning-icon">
+            <i className="fa-solid fa-triangle-exclamation"></i>
+          </div>
+          <div className='notify'>Password Length Should Be Above 6</div>
+        </div>
+
+        <div className={`notifications-checkbox ${showNotificationCheckbox ? " active-notifications-checkbox " : ''}`}>
+          <div className="warning-icon">
+            <i className="fa-solid fa-triangle-exclamation"></i>
+          </div>
+          <div className='notify'>Please Select Atleast One Of Them</div>
+        </div>
+
+        <div className={`notifications-copied ${showNotificationCopied ? " active-notifications-copied " : ''}`}>
+          <div className="copied-icon">
+            <i className="fa-solid fa-check"></i>
+          </div>
+          <div className='notify'>Copied</div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </>
+  )
 }
+
+export default page
